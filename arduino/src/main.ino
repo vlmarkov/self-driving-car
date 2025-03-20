66c2d80a1b7a63763e03fe74afe7a75ab558b33f
Vladislav Markov <vl.markv@yandex.ru>
2025-03-20 17:04:34 +0300

    Update main.ino

--- arduino/src/main.ino
+++ arduino/src/main.ino
@@ -1,7 +1,32 @@
+#include <pins.h>
+#include <motor_commands.h>
+
 void setup() {
-    // TODO: put your setup code here, to run once!
+    pinMode(ENGINE_LEFT_PIN, OUTPUT);
+    pinMode(ENGINE_LEFT_HIGH_PIN, OUTPUT);
+    pinMode(ENGINE_LEFT_LOW_PIN, OUTPUT);
+    
+    pinMode(ENGINE_RIGHT_PIN, OUTPUT);
+    pinMode(ENGINE_RIGHT_HIGH_PIN, OUTPUT);
+    pinMode(ENGINE_RIGHT_LOW_PIN, OUTPUT);
+
+    pinMode(FROM_RAPSBERRY_PIN_0, INPUT_PULLUP);
+    pinMode(FROM_RAPSBERRY_PIN_1, INPUT_PULLUP);
+    pinMode(FROM_RAPSBERRY_PIN_2, INPUT_PULLUP);
 }
   
 void loop() {
-    // TODO: put your main code here, to run repeatedly!
+    int is_stop = digitalRead(FROM_RAPSBERRY_PIN_0);
+    int is_forward = digitalRead(FROM_RAPSBERRY_PIN_1);
+    int is_left_turn = digitalRead(FROM_RAPSBERRY_PIN_0);
+
+    MotorCommands mc = get_motor_commands(is_stop, is_forward, is_left_turn);
+
+    digitalWrite(ENGINE_LEFT_HIGH_PIN, mc.engine_left_high);
+    digitalWrite(ENGINE_LEFT_LOW_PIN, mc.engine_left_low);
+    digitalWrite(ENGINE_RIGHT_HIGH_PIN, mc.engine_right_high);
+    digitalWrite(ENGINE_RIGHT_LOW_PIN, mc.engine_right_low);
+    
+    analogWrite(ENGINE_LEFT_PIN, mc.engine_left_voltage);
+    analogWrite(ENGINE_RIGHT_PIN, mc.engine_right_voltage);
 }
