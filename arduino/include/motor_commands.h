29ddb8d5c9f6a0db948f58bcf522148acc7dd0d1
Vladislav Markov <vl.markv@yandex.ru>
2025-03-20 16:59:13 +0300

    Update motor_commands.h

--- arduino/include/motor_commands.h
+++ arduino/include/motor_commands.h
@@ -1,1 +1,12 @@
+#pragma once
 
+struct MotorCommands {
+    bool engine_left_high{true};
+    bool engine_left_low{false};
+    bool engine_right_high{true};
+    bool engine_right_low{false}
+    int engine_left_voltage{0};
+    int engine_right_voltage{0};
+};
+
+MotorCommands get_motor_commands(int is_stop, int is_forward, int is_left_turn);
