a5c428c7f6d1b27eb570ed459dc4fe8e1b9b21ed
Vladislav Markov <vl.markv@yandex.ru>
2025-03-20 16:57:22 +0300

    Create pins.h

---
+++ arduino/include/pins.h
@@ -0,0 +1,14 @@
+// Left side of Motors
+const int ENGINE_LEFT_PIN = 5; 
+const int ENGINE_LEFT_HIGH_PIN = 7;
+const int ENGINE_LEFT_LOW_PIN = 11;
+
+// Right side of Motors
+const int ENGINE_RIGHT_PIN = 6;
+const int ENGINE_RIGHT_HIGH_PIN = 9;
+const int ENGINE_RIGHT_LOW_PIN = 8;
+
+// Raspberry Pi connection
+const int FROM_RAPSBERRY_PIN_0 = 0; // Actual Raspberry Pi pin 21
+const int FROM_RAPSBERRY_PIN_1 = 1; // Actual Raspberry Pi pin 22
+const int FROM_RAPSBERRY_PIN_2 = 2; // Actual Raspberry Pi pin 23
