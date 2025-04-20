394c3b55c2d6e5c67bf76ae117c15fa61c4e665b
Vladislav Markov <vl.markv@yandex.ru>
2025-04-20 11:15:25 +0300

    feat: add inital raspberry camery support

--- ros2-modules/lane-detection/src/lane_detection_app.cpp
+++ ros2-modules/lane-detection/src/lane_detection_app.cpp
@@ -1,5 +1,38 @@
 #include <lane-detection/lane_detection_module.h>
 
+#include <raspicam/raspicam_cv.h>
+
+void setup_camera (int argc, char **argv, raspicam::RaspiCam_Cv &camera) {
+    camera.set(cv::CAP_PROP_FRAME_WIDTH,  ("-w", argc, argv,   400));
+    camera.set(cv::CAP_PROP_FRAME_HEIGHT, ("-h", argc, argv,   240));
+    camera.set(cv::CAP_PROP_BRIGHTNESS,   ("-br", argc, argv,  50));
+    camera.set(cv::CAP_PROP_CONTRAST,     ("-co", argc, argv,  50));
+    camera.set(cv::CAP_PROP_SATURATION,   ("-sa", argv, argc,  50));
+    camera.set(cv::CAP_PROP_GAIN,         ("-g", argc, argv,   50));
+    camera.set(cv::CAP_PROP_FPS,          ("-fps", argc, argv, 0));
+}
+
+void open_camera(raspicam::RaspiCam_Cv &camera) {
+    if (!camera.open()) {
+        std::cerr << "Error opening the camera" << std::endl;
+        std::exit(-1);
+    }
+}
+
+void stop_camera(raspicam::RaspiCam_Cv &camera) {
+    std::cout << "Stop camera..." << std::endl;
+    camera.release();
+}
+
+cv::Mat get_frame(raspicam::RaspiCam_Cv &camera) {
+    cv::Mat frame;
+
+    camera.grab();
+    camera.retrieve(frame);
+
+    return frame;
+}
+
 int main(int argc, char* argv[]) {
     auto frame = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
     if (frame.empty()) {
