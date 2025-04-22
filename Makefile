dfe9f98ed81276813fe4bc378f4374f4b1aacca1
Vladislav Markov <vl.markv@yandex.ru>
2025-04-22 20:50:50 +0300

    fix camery build if def condition

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ ENABLE_COMPILER_OPTIONS    := ON
 ENABLE_LATEST_CPP_STANDARD := ON
 # To debug purpose on your laptop (NOT Raspberry board) you can disable gpio write feature
 ENABLE_WIRING_PI_LIB       := ON
+ENABLE_RASPBERY_BUILD      := OFF
 
 all: interfaces common lane-detection manual-control obstacle-avoidance path-planning motion-calibration main-pipeline arduino-code
 
@@ -27,7 +28,7 @@ main-pipeline:
 	colcon build --packages-select main-pipeline --cmake-args -DENABLE_TESTS=$(ENABLE_TESTS) -DENABLE_COMPILER_OPTIONS=$(ENABLE_COMPILER_OPTIONS) -DENABLE_LATEST_CPP_STANDARD=$(ENABLE_LATEST_CPP_STANDARD)
 
 lane-detection:
-	colcon build --packages-select lane-detection --cmake-args -DENABLE_TESTS=$(ENABLE_TESTS) -DENABLE_COMPILER_OPTIONS=$(ENABLE_COMPILER_OPTIONS) -DENABLE_LATEST_CPP_STANDARD=$(ENABLE_LATEST_CPP_STANDARD)
+	colcon build --packages-select lane-detection --cmake-args -DENABLE_TESTS=$(ENABLE_TESTS) -DENABLE_COMPILER_OPTIONS=$(ENABLE_COMPILER_OPTIONS) -DENABLE_LATEST_CPP_STANDARD=$(ENABLE_LATEST_CPP_STANDARD) -DENABLE_RASPBERY_BUILD=$(ENABLE_RASPBERY_BUILD)
 
 manual-control:
 	colcon build --packages-select manual-control --cmake-args -DENABLE_TESTS=$(ENABLE_TESTS) -DENABLE_COMPILER_OPTIONS=$(ENABLE_COMPILER_OPTIONS) -DENABLE_LATEST_CPP_STANDARD=$(ENABLE_LATEST_CPP_STANDARD)
--- ros2-modules/lane-detection/src/lane_detection_app.cpp
+++ ros2-modules/lane-detection/src/lane_detection_app.cpp
@@ -1,17 +1,21 @@
 #include <lane-detection/lane_detection_module.h>
 
+#ifdef ENABLE_RASPBERY_BUILD
+
 #include <lccv.hpp>
 #include <libcamera_app.hpp>
 #include <opencv2/opencv.hpp>
 
-int test_camera() {
-	uint32_t num_cams = LibcameraApp::GetNumberCameras();
-	std::cout << "Found " << num_cams << " cameras." << std::endl;
+int test_raspbery_camera() {
+    uint32_t num_cams = LibcameraApp::GetNumberCameras();
+    std::cout << "Found " << num_cams << " cameras." << std::endl;
 
     uint32_t height = 720;
     uint32_t width = 1280;
-    std::cout<<"Sample program for LCCV video capture"<<std::endl;
-    std::cout<<"Press ESC to stop."<<std::endl;
+
+    std::cout << "Sample program for LCCV video capture" << std::endl;
+    std::cout << "Press ESC to stop." << std::endl;
+
     cv::Mat image = cv::Mat(height, width, CV_8UC3);
     lccv::PiCamera cam;
     cam.options->video_width=width;
@@ -20,31 +24,30 @@ int test_camera() {
     cam.options->verbose=true;
     cam.startVideo();
 
-
-    int ch=0;
+    int ch = 0;
     LaneDetectionModule lm({});
 
-    while(ch!=27){
-        if (!cam.getVideoFrame(image,1000)){
+    while (ch != 27) {
+        if (!cam.getVideoFrame(image,1000)) {
             std::cout<<"Timeout error"<<std::endl;
         } else {
-          auto frame = image;
-          auto status = lm.detect_lane(std::move(frame));
-          cv::imshow("Lane Detection Status", status.frame);
-          std::cout << status.direction << std::endl;
-          std::cout << status.steer_angle << std::endl;
-	    
-	}
-        ch=cv::waitKey(5);
+            auto frame = image;
+            auto status = lm.detect_lane(std::move(frame));
+            cv::imshow("Lane Detection Status", status.frame);
+            std::cout << status.direction << std::endl;
+            std::cout << status.steer_angle << std::endl;
+        }
+
+        ch = cv::waitKey(5);
     }
 
     cam.stopVideo();
     cv::destroyAllWindows();
 }
 
-int main(int argc, char* argv[]) {
-    test_camera();
+#endif // ENABLE_RASPBERY_BUILD
 
+int test_static_image(int argc, char* argv[]) {
     auto frame = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
     if (frame.empty()) {
         return -1;
@@ -60,6 +63,14 @@ int main(int argc, char* argv[]) {
     if (cv::waitKey(3000) >= 0) {
         return 0;
     }
+}
+
+int main(int argc, char* argv[]) {
+#ifdef ENABLE_RASPBERY_BUILD
+    test_raspbery_camera();
+#endif // ENABLE_RASPBERY_BUILD
+
+    test_static_image(argc, argv);
 
     return 0;
 }
